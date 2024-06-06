3eb7ea7746311ff47cbb1ab6c6cbff8a5c07ef6b
vivekanand-vr <bendragon21@gmail.com>
2024-06-06 11:06:18 +0530

    Add Spring Security with allow all requests.

---
+++ RentifyServer/src/main/java/in/rentify/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package in.rentify.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        @SuppressWarnings("deprecation")
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user").password("root123").roles("admin").build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+            .anyRequest().permitAll())
+            .csrf().disable();
+        return http.build();
+    }
+}
