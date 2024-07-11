fc1b828841d4c5eb7240ce02c32e6941a4be1fe9
vivekanand-vr <bendragon21@gmail.com>
2024-07-11 23:44:09 +0530

    Feat: Responsive home page and add navbar dropdown

--- RentifyClient/src/Pages/Home.jsx
+++ RentifyClient/src/Pages/Home.jsx
@@ -8,16 +8,18 @@ const Home = () => {
         navigate('/properties');
     }
     return (
-        <div className="w-full h-screen overflow-hidden bg-[url('./Assets/Home.jpg')] bg-cover bg-center">
-            <div className="relative flex mt-[-100px] justify-center items-center h-full">
-                <div className="bg-[rgba(0,0,0,0.7)] max-w-[900px] text-center p-5 rounded-lg">
-                    <h1 className='text-[white] font-extrabold text-[4rem] mb-2'>Renting Made Simple</h1>
-                    <p className='text-[white] text-[1.5rem] '>
+        <div className="w-full h-[600px] overflow-hidden bg-[url('./Assets/Home.jpg')] bg-cover bg-center md:h-screen">
+            <div className="relative flex justify-center items-center md:h-[500px] md:mt-28">
+                <div className="w-80 mt-12 bg-[rgba(0,0,0,0.7)] text-center p-4 rounded-lg md:w-[900px]">
+                    <h1 className="text-2xl text-white font-sans font-bold my-3 md:text-[4rem]">Renting Made Simple</h1>
+                    <p className="text-md text-white md:text-2xl font-sans md:mt-10">
                         We simplify the rental process for both property owners and tenants. 
                         In the post-pandemic world, the demand for real estate has surged, especially in cities with high populations and IT offices. <br />
                         RENTIFY aims to bridge the gap between property owners and tenants by providing a platform where they can easily connect based on their requirements.
                     </p>
-                    <button className='landing-page-button' onClick={handleOnClick}>View Properties</button>
+                <div className="flex justify-center mt-4">
+                    <button className="landing-page-button" onClick={handleOnClick}>View Properties</button>
+                </div>
                 </div>
             </div>
         </div>
