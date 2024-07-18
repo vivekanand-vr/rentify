054744dd5b46312117a1d8b49d51ae0f67275556
vivekanand-vr <bendragon21@gmail.com>
2024-07-18 10:53:40 +0530

    Fix: Implement duplicate email check in Signin-form & new endpoints

--- RentifyClient/src/Pages/SignIn.jsx
+++ RentifyClient/src/Pages/SignIn.jsx
@@ -36,89 +36,96 @@ const SignIn = () => {
       password: ''
     },
     validationSchema,
-    onSubmit: values => {
-      axios.post(API_ENDPOINTS.user.signin, values)
-        .then(response => {
-          toast.success('You have signed in sucessfully');
+    onSubmit: async (values) => {
+      try {
+        // Check if email is already registered
+        const emailCheckResponse = await axios.post(API_ENDPOINTS.user.checkEmail, { email: values.email });
+        if (emailCheckResponse.data.exists) {
+          toast.error('Email is already registered.');
+          return;
+        }
 
-          /* Updating the Login State after Signup & storing user details */
-          dispatch(userLogin(response.data));
-          setTimeout(() => { navigate('/'); }, 2000); 
-        })
-        .catch(error => {
-          console.error('There was an error Signing in:', error);
-          toast.error('There was an error Signing in.')
-        });
+        // Proceed with registration if email is not registered
+        const response = await axios.post(API_ENDPOINTS.user.signin, values);
+        toast.success('You have signed in successfully');
+
+        /* Updating the Login State after Signup & storing user details */
+        dispatch(userLogin(response.data));
+        setTimeout(() => { navigate('/'); }, 2000); 
+      } catch (error) {
+        console.error('There was an error Signing in:', error);
+        toast.error('There was an error Signing in.');
+      }
     }
   });
 
   return (
-      <div className="flex justify-center mx-[auto] my-6 p-4">
-        <div className="bg-white p-3 border-1 border-black rounded-lg w-[450px]">
-          <h2 className='font-nunito text-center font-bold mb-3 text-3xl md:text-4xl'>SIGN IN</h2>
+    <div className="flex justify-center mx-[auto] my-6 p-4">
+      <div className="bg-white p-3 border-1 border-black rounded-lg w-[450px]">
+        <h2 className='font-nunito text-center font-bold mb-3 text-3xl md:text-4xl'>SIGN IN</h2>
 
-          <span className='block text-center font-medium mb-4'>Already have an account ? <Link className='text-blue-700 font-medium' to={"/login"}>Login</Link></span>
+        <span className='block text-center font-medium mb-4'>Already have an account? <Link className='text-blue-700 font-medium' to={"/login"}>Login</Link></span>
+        
+        <form onSubmit={formik.handleSubmit}>
+          <div className="mb-4">
+            <label className='flex items-center ml-3 mb-1 font-medium'>First Name <CgProfile className='ml-1 text-xl' /></label>
+            <input className='w-[calc(100%-20px)] p-2 border-1 border-slate-400 rounded-sm ml-3' 
+                  type="text" name="firstName" value={formik.values.firstName}
+                  onChange={formik.handleChange} onBlur={formik.handleBlur} required />
+            {formik.touched.firstName && formik.errors.firstName ? 
+            (<div className="text-[red] text-center mx-0 my-[5px]">{formik.errors.firstName}</div>) : null}
+          </div>
+
+          <div className="mb-4">
+            <label className='flex items-center ml-3 mb-1 font-medium'>Last Name <CgProfile className='ml-1 text-xl' /></label>
+            <input className='w-[calc(100%-20px)] p-2 border-1 border-slate-400 rounded-sm ml-3'
+                   type="text" name="lastName" value={formik.values.lastName} 
+                   onChange={formik.handleChange} onBlur={formik.handleBlur} required />
+          </div>
+
+          <div className="mb-4">
+            <label className='flex items-center ml-3 mb-1 font-medium'>Email <MdOutlineMail className='ml-1 text-xl' /></label>
+            <input className='w-[calc(100%-20px)] p-2 border-1 border-slate-400 rounded-sm ml-3'
+                  type="email" name="email" value={formik.values.email} 
+                  onChange={formik.handleChange} onBlur={formik.handleBlur} required />
+            {formik.touched.email && formik.errors.email ? 
+            (<div className="text-[red] text-center mx-0 my-[5px]">{formik.errors.email}</div>) : null}
+          </div>
+
+          <div className="mb-4">
+            <label className='flex items-center ml-3 mb-1 font-medium'>Phone Number <IoCallOutline className='ml-2' /></label>
+            <input className='w-[calc(100%-20px)] p-2 border-1 border-slate-400 rounded-sm ml-3'
+            type="text" name="phoneNumber" value={formik.values.phoneNumber} 
+                  onChange={formik.handleChange} onBlur={formik.handleBlur} required />
+            {formik.touched.phoneNumber && formik.errors.phoneNumber ? 
+            (<div className="text-[red] text-center mx-0 my-[5px]">{formik.errors.phoneNumber}</div>) : null}
+          </div>
+
+          <div className="mb-4">
+            <label className='flex items-center ml-3 mb-1 font-medium'>City <MdOutlineLocationOn className='ml-1 text-xl' /></label>
+            <input className='w-[calc(100%-20px)] p-2 border-1 border-slate-400 rounded-sm ml-3'
+                   type="text" name="city" value={formik.values.city} 
+                   onChange={formik.handleChange} onBlur={formik.handleBlur} required />
+          </div>
+
+          <div className="mb-4">
+            <label className='flex items-center ml-3 mb-1 font-medium'>Set Password <RiLockPasswordLine className='ml-1 text-xl' /></label>
+            <input className='w-[calc(100%-20px)] p-2 border-1 border-slate-400 rounded-sm ml-3'
+                   type="password" name="password" value={formik.values.password} 
+                   onChange={formik.handleChange} onBlur={formik.handleBlur} required />
+            {formik.touched.password && formik.errors.password ? 
+            (<div className="text-[red] text-center mx-0 my-[5px]">{formik.errors.password}</div>) : null}
+          </div>
           
-          <form onSubmit={formik.handleSubmit}>
-            <div className="mb-4">
-              <label className='flex items-center ml-3 mb-1 font-medium'>First Name <CgProfile className='ml-1 text-xl' /> </label>
-              <input className='w-[calc(100%-20px)] p-2 border-1 border-slate-400 rounded-sm ml-3' 
-                    type="text" name="firstName" value={formik.values.firstName}
-                    onChange={formik.handleChange} onBlur={formik.handleBlur} required />
-              {formik.touched.firstName && formik.errors.firstName ? 
-              (<div className="text-[red] text-center mx-0 my-[5px]">{formik.errors.firstName}</div>) : null}
-            </div>
+          <div className='text-center'>
+            <button className='bg-blue-700 w-1/2 md:w-2/5 p-2 text-[white] border-[none] rounded-md cursor-pointer ml-3 mb-2
+                               hover:bg-slate-700'
+                    type="submit">Register</button>
+          </div>
 
-            <div className="mb-4">
-              <label className='flex items-center ml-3 mb-1 font-medium'>Last Name <CgProfile className='ml-1 text-xl' /></label>
-              <input className='w-[calc(100%-20px)] p-2 border-1 border-slate-400 rounded-sm ml-3'
-                     type="text" name="lastName" value={formik.values.lastName} 
-                     onChange={formik.handleChange} onBlur={formik.handleBlur} required />
-            </div>
-
-            <div className="mb-4">
-              <label className='flex items-center ml-3 mb-1 font-medium'>Email <MdOutlineMail className='ml-1 text-xl' /></label>
-              <input className='w-[calc(100%-20px)] p-2 border-1 border-slate-400 rounded-sm ml-3'
-                    type="email" name="email" value={formik.values.email} 
-                    onChange={formik.handleChange} onBlur={formik.handleBlur} required />
-              {formik.touched.email && formik.errors.email ? 
-              ( <div className="text-[red] text-center mx-0 my-[5px]">{formik.errors.email}</div> ) : null}
-            </div>
-
-            <div className="mb-4">
-              <label className='flex items-center ml-3 mb-1 font-medium'>Phone Number <IoCallOutline className='ml-2' /> </label>
-              <input className='w-[calc(100%-20px)] p-2 border-1 border-slate-400 rounded-sm ml-3'
-              type="text" name="phoneNumber" value={formik.values.phoneNumber} 
-                    onChange={formik.handleChange} onBlur={formik.handleBlur} required />
-              {formik.touched.phoneNumber && formik.errors.phoneNumber ? 
-              ( <div className="text-[red] text-center mx-0 my-[5px]">{formik.errors.phoneNumber}</div> ) : null}
-            </div>
-
-            <div className="mb-4">
-              <label className='flex items-center ml-3 mb-1 font-medium'>City <MdOutlineLocationOn className='ml-1 text-xl' /></label>
-              <input className='w-[calc(100%-20px)] p-2 border-1 border-slate-400 rounded-sm ml-3'
-                     type="text" name="city" value={formik.values.country} 
-                     onChange={formik.handleChange} onBlur={formik.handleBlur} required />
-            </div>
-
-            <div className="mb-4">
-              <label className='flex items-center ml-3 mb-1 font-medium'>Set Password <RiLockPasswordLine className='ml-1 text-xl' /></label>
-              <input className='w-[calc(100%-20px)] p-2 border-1 border-slate-400 rounded-sm ml-3'
-                     type="password" name="password" value={formik.values.password} 
-                     onChange={formik.handleChange} onBlur={formik.handleBlur} required />
-              {formik.touched.password && formik.errors.password ? 
-              ( <div className="text-[red] text-center mx-0 my-[5px]">{formik.errors.password}</div> ) : null}
-            </div>
-            
-            <div className='text-center'>
-              <button className='bg-blue-700 w-1/2 md:w-2/5 p-2 text-[white] border-[none] rounded-md cursor-pointer ml-3 mb-2
-                                 hover:bg-slate-700'
-                      type="submit">Register</button>
-            </div>
-
-          </form>
-        </div>
+        </form>
       </div>
+    </div>
   );
 }
 
--- RentifyClient/src/Services/Endpoints.jsx
+++ RentifyClient/src/Services/Endpoints.jsx
@@ -4,6 +4,7 @@ export const API_ENDPOINTS = {
   user: {
       login: `${BASE_URL}/user/login`,
       signin: `${BASE_URL}/user/signin`,
+      checkEmail: `${BASE_URL}/user/check-email`,
       details: `${BASE_URL}/user/`,
       update: `${BASE_URL}/user`,
   },
