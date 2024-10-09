c416fb94144405a551ff0f7203584b62eab48c7c
vivekanand-vr <bendragon21@gmail.com>
2024-10-09 15:07:09 +0530

    Refactor: Update PropertyList component for backend pagination and filtering

--- RentifyClient/src/Components/SortProperties.jsx
+++ RentifyClient/src/Components/SortProperties.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import { handleSort } from '../Services/Utils';
 
-const SortProperties = ({ properties, setProperties }) => {
+const SortProperties = ({ onSort }) => {
   const [dropDown, setDropDown] = useState(false);
 
   const toggleDropDown = () => setDropDown(!dropDown);
-  const closeDropDown = () => setDropDown(false); 
-  
+  const closeDropDown = () => setDropDown(false);
+
+  const handleSortClick = (criteria) => {
+    onSort(criteria);
+    closeDropDown();
+  };
+
   return (
     <div className="relative left-6 md:left-0 md:ml-52 mt-3">
       <button onClick={toggleDropDown}
@@ -16,28 +20,16 @@ const SortProperties = ({ properties, setProperties }) => {
 
       {dropDown && (
         <ul className="text-sm md:text-base absolute left-0 mt-2 w-72  bg-slate-200 shadow-lg z-50 rounded-md">
-          <li
-            onClick={() => { handleSort('Newest First', properties, setProperties); closeDropDown();}}
-            className="px-3 py-2 border-b border-gray-300 hover:bg-gray-500 hover:text-white cursor-pointer"
-          >
+          <li onClick={() => handleSortClick('datePosted,desc')} className="px-3 py-2 border-b border-gray-300 hover:bg-gray-500 hover:text-white cursor-pointer">
             Posted On (Newest First)
           </li>
-          <li
-            onClick={() => { handleSort('Oldest First', properties, setProperties); closeDropDown(); }}
-            className="px-3 py-2 border-b border-gray-300 hover:bg-gray-500 hover:text-white cursor-pointer"
-          >
+          <li onClick={() => handleSortClick('datePosted,asc')} className="px-3 py-2 border-b border-gray-300 hover:bg-gray-500 hover:text-white cursor-pointer">
             Posted On (Oldest First)
           </li>
-          <li
-            onClick={() => { handleSort('Rent Low to High', properties, setProperties); closeDropDown(); }}
-            className="px-3 py-2 border-b border-gray-300 hover:bg-gray-500 hover:text-white cursor-pointer"
-          >
+          <li onClick={() => handleSortClick('rent,asc')} className="px-3 py-2 border-b border-gray-300 hover:bg-gray-500 hover:text-white cursor-pointer">
             Rent (Low to High)
           </li>
-          <li
-            onClick={() => { handleSort('Rent High to Low', properties, setProperties); closeDropDown(); }}
-            className="px-3 py-2 border-b border-gray-300 hover:bg-gray-500 hover:text-white cursor-pointer"
-          >
+          <li onClick={() => handleSortClick('rent,desc')} className="px-3 py-2 border-b border-gray-300 hover:bg-gray-500 hover:text-white cursor-pointer">
             Rent (High to Low)
           </li>
         </ul>
